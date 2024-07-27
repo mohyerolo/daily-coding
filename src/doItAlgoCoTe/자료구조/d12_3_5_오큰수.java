7e13f5ad74a6dc3b0078475a318e00c62fe7d6cf
mohyerolo <garam2101@naver.com>
2024-07-27 16:02:20 +0900

    240727_doit알고리즘_자료구조_스택과큐_오큰수

---
+++ src/doItAlgoCoTe/자료구조/d12_3_5_오큰수.java
@@ -0,0 +1,40 @@
+package doItAlgoCoTe.자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class d12_3_5_오큰수 {
+    public static void main(String[] args) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] NGE = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 1; i < N; i++) {
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+                NGE[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            NGE[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : NGE) {
+            sb.append(num).append(' ');
+        }
+        System.out.println(sb);
+    }
+}
